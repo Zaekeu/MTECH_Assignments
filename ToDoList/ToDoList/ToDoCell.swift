667952a5ad17caa44b60d0cd40eb0a8e151b3292
Zaekeu <111462019+Zaekeu@users.noreply.github.com>
2022-11-15 08:14:28 -0700

    ToDoList

---
+++ ToDoList/ToDoList/ToDoCell.swift
@@ -0,0 +1,37 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Zach on 11/8/22.
+//
+
+import UIKit
+
+protocol ToDoCellDelegate: AnyObject {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+
+    @IBOutlet var isCompleteButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    
+    weak var delegate: ToDoCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func completeButtonTapped(_ sender: Any) {
+        delegate?.checkmarkTapped(sender: self)
+    }
+    
+
+}
