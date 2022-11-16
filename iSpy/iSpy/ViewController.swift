0f3d3ab2a07ce115346ee06fc6c7c6856118a164
Zaekeu <111462019+Zaekeu@users.noreply.github.com>
2022-11-16 07:33:59 -0700

    iSpy

---
+++ iSpy/iSpy/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  iSpy
+//
+//  Created by Zach on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+    
+//
+//    imageView.centerXAnchor.constraints(equalTo:
+//       scrollView.contentLayoutGuide.centerXAnchor)
+//    imageView.centerYAnchor.constraints(equalTo:
+//       scrollView.contentLayoutGuide.centerYAnchor)”
+
+}
+
